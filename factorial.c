98ff3da9bcc4c58e226753c3a0019360f22b4a1f
sushant054 <sushantnalage54@gmail.com>
2024-01-13 16:25:06 +0530

    compute n!

---
+++ factorial.c
@@ -0,0 +1,29 @@
+//compute..n!
+#include<stdio.h>
+int factorial(int n)
+{
+    int fact=1;
+    //if any one of them is true....
+    if(n==1 || n==0)
+        //fact of 1 and 0 is 1
+        return fact;
+    else
+    {
+        for(int i=1;i<=n;i++)
+        {
+            fact*=i;
+        }
+    }
+    return fact;
+}
+int main()
+{
+    int n,ans ;
+    printf("\nEnter n:");
+    scanf("%d",&n);
+    ans=factorial(n);
+    printf("Output: %d",ans);
+    return 0;
+}
+
+
