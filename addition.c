95bb23b609444ee457ef0f0436c00e3291dd0ada
sushant054 <sushantnalage54@gmail.com>
2024-01-14 17:50:18 +0530

    Addition of two numbers

---
+++ addition.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+ int main()
+ {
+    int a=0,b=0,ans;
+    printf("Enter values of a and b:");
+    scanf("%d%d",&a,&b);
+    ans=a+b;
+    printf("Addition of and b are:%d",ans);
+    return 0;
+ }
