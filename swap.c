aa4e3cb8c0a2a9727a91f2cfef455e87b55441b8
sushant054 <sushantnalage54@gmail.com>
2024-01-16 19:58:13 +0530

    swapping two no without using 3rd var...

---
+++ swap.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("Enter first no:");
+    scanf("%d",&a);
+    printf("Enter Second no:");
+    scanf("%d",&b);
+    //swap two numbers without using 3rd variable..... 
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("\nfirst no %d",a);
+    printf("\nsecond no %d",b);
+    return 0;
+}
