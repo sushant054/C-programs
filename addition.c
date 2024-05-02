5e4bd4d26169aa68cb0a170d538fda3dd115f59d
sushant054 <sushantnalage54@gmail.com>
2024-05-02 17:54:28 +0530

    Addition of two no..

--- addition.c
+++ addition.c
@@ -4,6 +4,7 @@
     int a=0,b=0,ans;
     printf("Enter values of a and b:");
     scanf("%d%d",&a,&b);
+    //addition of two no.
     ans=a+b;
     printf("Addition of and b are:%d",ans);
     return 0;
